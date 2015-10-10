5c519b5829ed452fed36373899d9010dfdfe4f6d
ubergesundheit <ubergesundheit@googlemail.com>
2015-10-10 20:17:04 +0200

    make map tiles, center and colors configurable

--- _includes/javascript/map.js
+++ _includes/javascript/map.js
@@ -30,39 +30,39 @@ L.extend(window.Weihnachtsmarkt, {
   },
   _initMap: function () {
     var map = L.map("map",{
-      center: [51.96144, 7.6292],
-      zoom: 18,
+      center: {{ site.map.center | jsonify }},
+      zoom: {{ site.map.zoom }},
       attributionControl: false
     });
 
     this._staendeStyles = {
       normal: {
-        color: "#f8f8f8",
+        color: "{{ site.map.normalColor }}",
         weight: 0,
         opacity: 1,
         fillOpacity: 0.8
       },
       high: {
-        color: "#ff3322",
+        color: "{{ site.map.highColor }}",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
       },
       low: {
-        color: "#B8BCC8",
+        color: "{{ site.map.lowColor }}",
         weight: 0,
         opacity: 0.5,
         fillOpacity: 0.5
       },
       result: {
-        color: "#F57F06",
+        color: "{{ site.map.resultColor }}",
         weight: 2,
         opacity: 1,
         fillOpacity: 1
       }
     };
 
-    L.tileLayer("http://{s}.tiles.mapbox.com/v3/tomrocket.k93e7pp4/{z}/{x}/{y}.png", {
+    L.tileLayer("{{ site.map.tileLayerUrl }}", {
       maxZoom: 22
     }).addTo(map);
 
