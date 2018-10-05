dffa860066b42c0cc293bfa52d5e2fab825cd381
felixerdy <felix.erdmann@uni-muenster.de>
2018-10-05 23:42:16 +0200

    styling fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -43,6 +43,7 @@ const Layout = ({ children }) => (
           <div
             style={{
               width: '100%',
+              overflow: 'scroll',
               // margin: '0 auto',
               // maxWidth: 960,
               // padding: '0px 1.0875rem 1.45rem',
