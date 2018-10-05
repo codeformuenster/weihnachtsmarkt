7ac29d167a52eac6da4dc4e6f0a7e0cc7b681ac9
Christian Römer <christian.roemer@udo.edu>
2018-10-05 18:38:49 +0200

    Okayish design

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -28,30 +28,22 @@ class Details extends Component {
 
     return (
       <div>
-        <div id={'container'}>
-          <header className={'header'}>
-            <h2>There will be a header</h2>
-          </header>
-          <div className={'title'}>{name}</div>
-          <div className={'imagearea'}>
-            <div className={'img'}>
-              <img
-                className={'fit'}
-                src={require('../../images/' + image)} // eslint-disable-line no-undef
-              />
-            </div>
+        <div id={'details-container'}>
+          <div className={'details-name'}>{name}</div>
+          <div className={'details-imagearea'}>
+            <img
+              className={'details-fit'}
+              src={require('../../images/' + image)} // eslint-disable-line no-undef
+            />
           </div>
-          <div className={'tags'}>
+          <div className={'details-tags'}>
             {tags.map(tag => (
-              <u key={tag} className={'tag'}>
+              <u key={tag} className={'details-tag'}>
                 {tag}
               </u>
             ))}
           </div>
-          <div className={'description'}>{description}</div>
-          <footer className={'searchbar'}>
-            <h3>There might be a searchbar</h3>
-          </footer>
+          <div className={'details-description'}>{description}</div>
         </div>
       </div>
     )
--- src/pages/details.js
+++ src/pages/details.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 import Details from '../components/Details/Details'
+import Layout from '../components/layout'
 
 const DetailsPage = () => (
-  <Details
-    title={'Wiesn Bratwurst & mehr'}
-    tags={['Leibliches Wohl', 'Unterhaltung']}
-    description={
-      'Beim "Wiesn Bratwurst & mehr" bekommt man die volle Dröhnung! Leckere bayerische Spezialitäten zu zünftiger Wiesn Musik vom Live Act \\"Die Wiesn-Boys\\". Die Tradition verpflichtet und wichtige Dinge werden nicht außer Acht gelassen. Für das leibliche Wohl ist gesorgt und keine Auge bleibt trocken. O\'zapft is!'
-    }
-  />
+  <Layout>
+    <Details
+      name={'Wiesn Bratwurst & mehr'}
+      tags={['Leibliches Wohl', 'Unterhaltung']}
+      description={
+        'Beim "Wiesn Bratwurst & mehr" bekommt man die volle Dröhnung! Leckere bayerische Spezialitäten zu zünftiger Wiesn Musik vom Live Act \\"Die Wiesn-Boys\\". Die Tradition verpflichtet und wichtige Dinge werden nicht außer Acht gelassen. Für das leibliche Wohl ist gesorgt und keine Auge bleibt trocken. O\'zapft is!'
+      }
+    />
+  </Layout>
 )
 
 export default DetailsPage
