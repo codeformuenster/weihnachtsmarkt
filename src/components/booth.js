f1b7096ef4ece6e2485704f69787f0311af21adc
jahnique <schencking@gmail.com>
2018-10-05 17:35:28 +0200

    Add first list view draft

--- src/components/booth.js
+++ src/components/booth.js
@@ -2,39 +2,26 @@
 import React from 'react'
 
 class Booth extends React.Component {
-  //constructor(props) {
-    //super(props)
-  //}
 
   render() {
-    //const { props } = this.props
-
     if (!this.props.name) {
       throw new Error('Your booth should have a name')
     }
 
     return (
       <div className="booth">
-        <div className="logo">
-          LOGO
+        <div className="logo ">
+          <i className="material-icons">map</i>
         </div>
-        <div className="name">
-          <p style={{ margin: 0 }}>
-            {this.props.name}
-          </p>
-        </div>
-        <div className="market">
-          {this.props.market}
-        </div>
-        <div className="tags">
-          {this.showTags(this.props.tags)}
-        </div>
+        <div className="name truncate">{this.props.name}</div>
+        <div className="market truncate">{this.props.market}</div>
+        <div className="tags truncate">{this.showTags(this.props.tags)}</div>
       </div>
     )
   }
 
-  showTags = (tags) => {
-    return tags.join(', ');
+  showTags = tags => {
+    return tags.join(', ')
   }
 }
 
