39be3f27e592ca8cd964a77043f0639cd15b4ae3
felixerdy <felix.erdmann@uni-muenster.de>
2018-10-05 12:32:50 +0200

    add css grid example

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,23 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import Map from '../components/map'
 
-const IndexPage = () => (
+import './index.css'
+
+const IndexPage = () => {
+  return (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className='grid'>
+      <h1>Hi people</h1>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+      <p className='ueberall'>Das ist breit</p>
+      <p className='mitte-unten'>Das ist mitte unten</p>
+
+    </div>
   </Layout>
-)
+  )
+}
 
 export default IndexPage
