e20485b65d9ca6d01770012ee008e3286797d6ca
felixerdy <felix.erdmann@uni-muenster.de>
2018-10-06 11:32:11 +0200

    load data one, preserve viewport

--- src/components/search.js
+++ src/components/search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './search.css'
 
 export default class Search extends Component {
@@ -9,8 +10,22 @@ export default class Search extends Component {
           className="search-input"
           type="search"
           placeholder="Suche nach Ständen, Produkten und Kategorien..."
+          onChange={e => {
+            // this.props.setFilterData(e.target.value)
+            fetch(
+              `https://kinto-weihnachtsmarkt.codeformuenster.org/v1/buckets/weihnachtsmarkt/collections/booths/search?q=*${
+                e.target.value
+              }*`
+            )
+              .then(response => response.json())
+              .then(data => this.props.setFilterData(data))
+          }}
         />
       </div>
     )
   }
+}
+
+Search.propTypes = {
+  setFilterData: PropTypes.func,
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,45 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import ConnectedMap from '../containers/Map/Map'
+import Map from '../components/Map/Map'
 import Search from '../components/search'
 
 import './index.css'
+import { connect } from 'react-redux'
+
+const mapStateToProps = ({ allBooths, allMarkets, viewport }) => ({
+  allBooths,
+  allMarkets,
+  viewport,
+})
+const mapDispatchToProps = dispatch => {
+  return {
+    setAllBooths: markets =>
+      dispatch({ type: 'SET_ALL_BOOTHS', payload: markets }),
+    setAllMarkets: markets =>
+      dispatch({ type: 'SET_ALL_MARKETS', payload: markets }),
+    setSelectedMarket: market =>
+      dispatch({ type: `SET_SELECTED_MARKET`, payload: market }),
+    setSelectedBooth: booth =>
+      dispatch({ type: `SET_SELECTED_BOOTH`, payload: booth }),
+    setViewport: viewport =>
+      dispatch({ type: `SET_VIEWPORT`, payload: viewport }),
+  }
+}
+const searchDispatchToProps = dispatch => {
+  return {
+    setFilterData: filterData =>
+      dispatch({ type: `SET_FILTER_DATA`, payload: filterData }),
+  }
+}
+const ConnectedMap = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Map)
+const ConnectedSearch = connect(
+  null,
+  searchDispatchToProps
+)(Search)
 
 const IndexPage = () => {
   return (
@@ -18,7 +53,7 @@ const IndexPage = () => {
           right: '0',
         }}
       >
-        <Search />
+        <ConnectedSearch />
       </div>
     </Layout>
   )
