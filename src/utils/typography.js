676199e64332f5c7f5a1f82f848d3697b2ff0610
Simon Schulte <simonschulte@posteo.net>
2018-10-30 20:56:19 +0100

    add: Google lato font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,24 @@
 import Typography from "typography"
 
 const typography = new Typography({
-  baseFontSize: "30px",
+  baseFontSize: "18px",
   baseLineHeight: 1.666,
   headerFontFamily: [
-    "Avenir Next",
-    "Helvetica Neue",
-    "Segoe UI",
-    "Helvetica",
-    "Arial",
-    "sans-serif",
+    "Lato"
   ],
-  bodyFontFamily: ["Georgia", "serif"],
+  bodyFontFamily: ["Lato"],
+  googleFonts: [
+    {
+      name: 'Lato',
+      styles: [
+        '300',
+        '400',
+        '400i',
+        '700',
+        '700i',
+      ],
+    },
+  ],
 })
 
 export default typography
