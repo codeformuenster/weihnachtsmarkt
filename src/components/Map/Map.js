ef8a74c56546da5ad43fa70eebbd07cc96296fc8
felixerdy <felix.erdmann@uni-muenster.de>
2018-12-04 20:28:34 +0100

    show booths on map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -8,6 +8,7 @@ import { Link } from 'gatsby'
 import ReactDOM from 'react-dom'
 import Legend from './Legend/Legend'
 import ReactMapGL, { NavigationControl } from 'react-map-gl'
+import { boothStyle, marketStyle } from './styling'
 
 import './map.css'
 
@@ -20,10 +21,14 @@ export default class Map extends Component {
   constructor(props) {
     super(props)
     this.addPopup = this.addPopup.bind(this)
+    this._onLoad = this._onLoad.bind(this)
   }
 
   async componentDidMount() {
+    await this.fetchData()
+
     this.map = this.themap.getMap()
+
     this.map.addControl(
       new mapboxgl.GeolocateControl({
         positionOptions: {
@@ -32,128 +37,7 @@ export default class Map extends Component {
         trackUserLocation: true,
       })
     )
-    this.map.on('style.load', async () => {
-      try {
-        let marketsFeatures = this.props.allMarkets
-        if (this.props.allMarkets.length === 0) {
-          await pois.sync()
-          const { data } = await pois.list({ filters: { type: 'market' } })
-          this.props.setAllMarkets(data)
-        }
-        this.map.addSource('markets-source', {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: marketsFeatures.map(e => ({
-              ...e,
-              type: 'Feature',
-              properties: e,
-            })),
-          },
-        })
-        this.map.addLayer({
-          id: 'markets',
-          type: 'fill',
-          source: 'markets-source',
-          layout: {},
-          maxzoom: 18,
-          paint: {
-            'fill-color': '#747474',
-            'fill-opacity': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              12,
-              0.8,
-              16,
-              0.6,
-              18,
-              0,
-            ],
-          },
-        })
-      } catch (err) {
-        // eslint-disable-next-line
-        console.log('error', err)
-      }
-      try {
-        let boothsFeatures = this.props.allBooths
-        if (this.props.allBooths.length === 0) {
-          await booths.sync()
-          const { data } = await booths.list()
-          this.props.setAllBooths(data)
-        }
-        this.map.addSource('booths-source', {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: boothsFeatures.map(e => ({
-              ...e,
-              type: 'Feature',
-              properties: {
-                ...e,
-                filterVisible: 0,
-              },
-            })),
-          },
-        })
-        this.map.addLayer({
-          id: 'booths',
-          type: 'fill-extrusion',
-          source: 'booths-source',
-          layout: {},
-          minzoom: 13,
-          paint: {
-            'fill-extrusion-color': [
-              'case',
-              // if filterVisible == 0
-              //color depending on type
-              ['==', ['get', 'filterVisible'], 0],
-              [
-                'match',
-                ['get', 'type'],
-                'beverage',
-                '#390035',
-                'craft',
-                '#0097df',
-                'food',
-                '#db5f62',
-                'clothes',
-                '#00D1B2',
-                'candy',
-                '#ffde2d',
-                /* other */ '#ccc',
-              ],
-              // else
-              //color white
-              ['==', ['get', 'filterVisible'], 1],
-              '#ffffff',
-              // default
-              // color white
-              '#ffffff',
-              // {
-              //   property: 'filterVisible',
-              //   stops: [[0, '#F60000'], [1, '#00FFFF']],
-              // },
-            ],
-            'fill-extrusion-opacity': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              13,
-              0,
-              16,
-              1,
-            ],
-            'fill-extrusion-height': 3,
-            // 'fill-extrusion-opacity': 0.5,
-          },
-        })
-      } catch (err) {
-        // eslint-disable-next-line
-        console.log('error', err)
-      }
-    })
+
     this.map.on('click', 'markets', e => {
       // eslint-disable-next-line
       console.log(e.features)
@@ -184,6 +68,69 @@ export default class Map extends Component {
       ).geometry.coordinates
       this.addPopup(<Popup />, coords[1], coords[0])
     })
+  }
+
+  async fetchData() {
+    if (this.props.allMarkets.length === 0) {
+      await pois.sync()
+      const { data } = await pois.list({ filters: { type: 'market' } })
+      this.props.setAllMarkets(data)
+    }
+
+    if (this.props.allBooths.length === 0) {
+      await booths.sync()
+      const { data } = await booths.list()
+      this.props.setAllBooths(data)
+    }
+  }
+
+  _onLoad() {
+    if (this.map != undefined) {
+      // add Markets from POI Collection
+      this.map.addSource('markets-source', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: this.props.allMarkets.map(e => ({
+            ...e,
+            type: 'Feature',
+            properties: e,
+          })),
+        },
+      })
+      this.map.addLayer({
+        id: 'markets',
+        type: 'fill',
+        source: 'markets-source',
+        layout: {},
+        maxzoom: 18,
+        paint: marketStyle,
+      })
+
+      // add Booths from Booth from Booth Collection
+      this.map.addSource('booths-source', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: this.props.allBooths.map(e => ({
+            ...e,
+            type: 'Feature',
+            properties: {
+              ...e,
+              filterVisible: 0,
+            },
+          })),
+        },
+      })
+      this.map.addLayer({
+        id: 'booths',
+        type: 'fill-extrusion',
+        source: 'booths-source',
+        layout: {},
+        minzoom: 13,
+        paint: boothStyle,
+      })
+    }
   }
 
   addPopup(el, lat, lng) {
@@ -227,117 +174,117 @@ export default class Map extends Component {
   }
 
   render() {
-    this.map = this.themap == undefined ? null : this.themap.getMap()
-    if (this.props.filterData.length !== 0 && this.map != null) {
-      this.map.getSource('booths-source').setData({
-        type: 'FeatureCollection',
-        features: this.props.allBooths.map(e => ({
-          ...e,
-          type: 'Feature',
-          properties: {
-            ...e,
-            filterVisible: this.props.filterData.includes(e.id) ? 1 : 0,
-          },
-        })),
-      })
-      this.map.setPaintProperty('booths', 'fill-extrusion-color', [
-        'case',
-        // if filterVisible == 0
-        //color depending on type
-        ['==', ['get', 'filterVisible'], 0],
-        'grey',
-        // [
-        //   'match',
-        //   ['get', 'type'],
-        //   'beverage',
-        //   '#bda4bb',
-        //   'craft',
-        //   '#c8dbe4',
-        //   'food',
-        //   '#d8babb',
-        //   'clothes',
-        //   '#aac3c0',
-        //   'candy',
-        //   '#fdfae8',
-        //   /* other */ '#ccc',
-        // ],
-        // else
-        //color white
-        ['==', ['get', 'filterVisible'], 1],
-        'red',
-        // [
-        //   'match',
-        //   ['get', 'type'],
-        //   'beverage',
-        //   '#390035',
-        //   'craft',
-        //   '#0097df',
-        //   'food',
-        //   '#db5f62',
-        //   'clothes',
-        //   '#00D1B2',
-        //   'candy',
-        //   '#ffde2d',
-        //   /* other */ '#ccc',
-        // ],
-        // default
-        // color white
-        '#ffffff',
-        // {
-        //   property: 'filterVisible',
-        //   stops: [[0, '#F60000'], [1, '#00FFFF']],
-        // },
-      ])
-    } else {
-      if (this.map != null) {
-        if (this.map.getSource('booths-source') != null) {
-          this.map.getSource('booths-source').setData({
-            type: 'FeatureCollection',
-            features: this.props.allBooths.map(e => ({
-              ...e,
-              type: 'Feature',
-              properties: {
-                ...e,
-                filterVisible: 0,
-              },
-            })),
-          })
-          // set default styling
-          this.map.setPaintProperty('booths', 'fill-extrusion-color', [
-            'case',
-            // if filterVisible == 0
-            //color depending on type
-            ['==', ['get', 'filterVisible'], 0],
-            [
-              'match',
-              ['get', 'type'],
-              'beverage',
-              '#390035',
-              'craft',
-              '#0097df',
-              'food',
-              '#db5f62',
-              'clothes',
-              '#00D1B2',
-              'candy',
-              '#ffde2d',
-              /* other */ '#ccc',
-            ],
-            // else
-            //color white
-            ['==', ['get', 'filterVisible'], 1],
-            '#ffffff',
-            // default
-            // color white
-            '#ffffff',
-            // {
-            //   property: 'filterVisible',
-            //   stops: [[0, '#F60000'], [1, '#00FFFF']],
-            // },
-          ])
-        }
-      }
-    }
+    // this.map = this.themap == undefined ? null : this.themap.getMap()
+    // if (this.props.filterData.length !== 0 && this.map != null) {
+    //   this.map.getSource('booths-source').setData({
+    //     type: 'FeatureCollection',
+    //     features: this.props.allBooths.map(e => ({
+    //       ...e,
+    //       type: 'Feature',
+    //       properties: {
+    //         ...e,
+    //         filterVisible: this.props.filterData.includes(e.id) ? 1 : 0,
+    //       },
+    //     })),
+    //   })
+    //   this.map.setPaintProperty('booths', 'fill-extrusion-color', [
+    //     'case',
+    //     // if filterVisible == 0
+    //     //color depending on type
+    //     ['==', ['get', 'filterVisible'], 0],
+    //     'grey',
+    //     // [
+    //     //   'match',
+    //     //   ['get', 'type'],
+    //     //   'beverage',
+    //     //   '#bda4bb',
+    //     //   'craft',
+    //     //   '#c8dbe4',
+    //     //   'food',
+    //     //   '#d8babb',
+    //     //   'clothes',
+    //     //   '#aac3c0',
+    //     //   'candy',
+    //     //   '#fdfae8',
+    //     //   /* other */ '#ccc',
+    //     // ],
+    //     // else
+    //     //color white
+    //     ['==', ['get', 'filterVisible'], 1],
+    //     'red',
+    //     // [
+    //     //   'match',
+    //     //   ['get', 'type'],
+    //     //   'beverage',
+    //     //   '#390035',
+    //     //   'craft',
+    //     //   '#0097df',
+    //     //   'food',
+    //     //   '#db5f62',
+    //     //   'clothes',
+    //     //   '#00D1B2',
+    //     //   'candy',
+    //     //   '#ffde2d',
+    //     //   /* other */ '#ccc',
+    //     // ],
+    //     // default
+    //     // color white
+    //     '#ffffff',
+    //     // {
+    //     //   property: 'filterVisible',
+    //     //   stops: [[0, '#F60000'], [1, '#00FFFF']],
+    //     // },
+    //   ])
+    // } else {
+    //   if (this.map != null) {
+    //     if (this.map.getSource('booths-source') != null) {
+    //       this.map.getSource('booths-source').setData({
+    //         type: 'FeatureCollection',
+    //         features: this.props.allBooths.map(e => ({
+    //           ...e,
+    //           type: 'Feature',
+    //           properties: {
+    //             ...e,
+    //             filterVisible: 0,
+    //           },
+    //         })),
+    //       })
+    //       // set default styling
+    //       this.map.setPaintProperty('booths', 'fill-extrusion-color', [
+    //         'case',
+    //         // if filterVisible == 0
+    //         //color depending on type
+    //         ['==', ['get', 'filterVisible'], 0],
+    //         [
+    //           'match',
+    //           ['get', 'type'],
+    //           'beverage',
+    //           '#390035',
+    //           'craft',
+    //           '#0097df',
+    //           'food',
+    //           '#db5f62',
+    //           'clothes',
+    //           '#00D1B2',
+    //           'candy',
+    //           '#ffde2d',
+    //           /* other */ '#ccc',
+    //         ],
+    //         // else
+    //         //color white
+    //         ['==', ['get', 'filterVisible'], 1],
+    //         '#ffffff',
+    //         // default
+    //         // color white
+    //         '#ffffff',
+    //         // {
+    //         //   property: 'filterVisible',
+    //         //   stops: [[0, '#F60000'], [1, '#00FFFF']],
+    //         // },
+    //       ])
+    //     }
+    //   }
+    // }
     return (
       <div style={{ height: '100%', position: 'relative' }}>
         <div className="welcome-sign">
@@ -349,8 +296,8 @@ export default class Map extends Component {
           width="100%"
           height="100%"
           onViewportChange={viewport => this.props.setViewport(viewport)}
+          onLoad={this._onLoad}
           mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
-          reuseMaps={true}
           mapStyle="mapbox://styles/felixaetem/cjmwkrak403hr2snrjunbuvze"
         >
           <div
---
+++ src/components/Map/styling.js
@@ -0,0 +1,52 @@
+export const boothStyle = {
+  'fill-extrusion-color': [
+    'case',
+    // if filterVisible == 0
+    //color depending on type
+    ['==', ['get', 'filterVisible'], 0],
+    [
+      'match',
+      ['get', 'type'],
+      'beverage',
+      '#390035',
+      'craft',
+      '#0097df',
+      'food',
+      '#db5f62',
+      'clothes',
+      '#00D1B2',
+      'candy',
+      '#ffde2d',
+      /* other */ '#ccc',
+    ],
+    // else
+    //color white
+    ['==', ['get', 'filterVisible'], 1],
+    '#ffffff',
+    // default
+    // color white
+    '#ffffff',
+    // {
+    //   property: 'filterVisible',
+    //   stops: [[0, '#F60000'], [1, '#00FFFF']],
+    // },
+  ],
+  'fill-extrusion-opacity': ['interpolate', ['linear'], ['zoom'], 13, 0, 16, 1],
+  'fill-extrusion-height': 3,
+  // 'fill-extrusion-opacity': 0.5,
+}
+
+export const marketStyle = {
+  'fill-color': '#747474',
+  'fill-opacity': [
+    'interpolate',
+    ['linear'],
+    ['zoom'],
+    12,
+    0.8,
+    16,
+    0.6,
+    18,
+    0,
+  ],
+}
